b4f874fd781cc3e5b7b487d849effa837b9ce5ea
jace200677 <jacefink17@outlook.com>
2026-01-28 21:06:56 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, Response
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def portal():
-    return render_template('index.html')  # Loads templates/index.html
+    # Fetch remote HTML from GitHub Pages
+    r = requests.get('https://jace200677.github.io/free-wifi/templates/index.html')
+    return Response(r.text, mimetype='text/html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
